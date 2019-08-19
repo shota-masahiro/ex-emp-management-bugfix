75d7b44ed2334da390dc57427e9cae6da536450c
shotasuzuki <shota.suzuki.zv@rakus-partners.co.jp>
2019-08-19 17:05:00 +0900

    (1-2)初級 入力値エラー処理の修正が完了しました。

--- src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
+++ src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
@@ -1,5 +1,9 @@
 package jp.co.sample.emp_management.form;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
@@ -8,11 +12,18 @@ package jp.co.sample.emp_management.form;
  */
 public class InsertAdministratorForm {
 	/** 名前 */
+	@NotBlank(message = "氏名は必須です")
 	private String name;
+	
 	/** メールアドレス */
+	@Size(min = 1, max = 127, message = "Eメールは1文字以上127文字以内で記載してください")
+	@Email(message = "Eメールの形式が不正です")
 	private String mailAddress;
+	
 	/** パスワード */
+	@Size(min = 1, max = 127, message = "パスワードは1文字以上127文字以内で記載してください")
 	private String password;
+	
 	public String getName() {
 		return name;
 	}
